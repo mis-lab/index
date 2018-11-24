c248589586fd54b443524f23975ede5c6f38a535
hanyuxiao <hanorz@foxmail.com>
2018-11-24 22:22:22 +0800

    refactor：重构 Spring bean 注入方式

--- src/main/java/cn/edu/cqupt/mis/index/Application.java
+++ src/main/java/cn/edu/cqupt/mis/index/Application.java
@@ -2,12 +2,19 @@ package cn.edu.cqupt.mis.index;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource(locations = "classpath:spring-bean.xml")
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
     }
 }
